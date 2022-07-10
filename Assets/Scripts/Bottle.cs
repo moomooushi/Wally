a542505bb92c7934eae5772f38dbdc03e9f9ed48
solidfry <ellioteweb@gmail.com>
2022-07-10 13:40:17 +1000

    Updated colliders for bottles

--- Assets/Scripts/Bottle.cs
+++ Assets/Scripts/Bottle.cs
@@ -27,12 +27,15 @@ public class Bottle : Receptacle
     {
         if (fluidPrefab != null)
         {
+            var rb = this.GetComponent<Rigidbody2D>();
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
             for (int i = 0; i < fillCount; i++)
             {
                 GameObject _ = Instantiate(fluidPrefab, receptacleTransform.position, Quaternion.identity, receptacleTransform);
                 Fluid fluid = _.GetComponent<Fluid>();
                 fluid.fluidType = fluidType;
             }
+            rb.constraints = RigidbodyConstraints2D.None;
         }
     }
 }
