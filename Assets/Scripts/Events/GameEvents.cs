312648a95c102cad0a5524f60849c28e5345ff35
solidfry <ellioteweb@gmail.com>
2022-07-13 11:33:01 +1000

    improved level loading by using events

--- Assets/Scripts/Events/GameEvents.cs
+++ Assets/Scripts/Events/GameEvents.cs
@@ -6,11 +6,12 @@ namespace Events
     {
         public delegate void FluidInGlass(IngredientType ingredientType);
         public delegate void FluidExitGlass(IngredientType ingredientType);
-
         public delegate void DetermineLevelCompletion();
+        public delegate void LevelCompleted();
         
         public static FluidInGlass OnIngredientEnterGlassEvent;
         public static FluidExitGlass OnIngredientExitGlassEvent;
         public static DetermineLevelCompletion OnIngredientUpdatedEvent;
+        public static LevelCompleted OnLevelCompletedEvent;
     }
 }
