57d8ed2539e6af5eec1cf1ddd2740d0c0e38ad99
solidfry <ellioteweb@gmail.com>
2022-07-13 13:43:39 +1000

    playerdata SO

--- Assets/Scripts/Events/GameEvents.cs
+++ Assets/Scripts/Events/GameEvents.cs
@@ -8,10 +8,12 @@ namespace Events
         public delegate void FluidExitGlass(IngredientType ingredientType);
         public delegate void DetermineLevelCompletion();
         public delegate void LevelCompleted();
+        public delegate void UpdateWallet(float valueToAdd);
         
         public static FluidInGlass OnIngredientEnterGlassEvent;
         public static FluidExitGlass OnIngredientExitGlassEvent;
         public static DetermineLevelCompletion OnIngredientUpdatedEvent;
         public static LevelCompleted OnLevelCompletedEvent;
+        public static UpdateWallet OnUpdateWalletEvent;
     }
 }
