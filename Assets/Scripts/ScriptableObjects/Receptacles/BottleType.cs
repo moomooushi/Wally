1839e597fc195f738fa9ab16608a78db53e04883
solidfry <ellioteweb@gmail.com>
2022-07-22 11:27:34 +1000

    Asset additions
    Water sounds
    GPU instancing Sprite Unlit shader to help performance

--- Assets/Scripts/ScriptableObjects/Receptacles/BottleType.cs
+++ Assets/Scripts/ScriptableObjects/Receptacles/BottleType.cs
@@ -5,7 +5,7 @@ namespace ScriptableObjects.Receptacles
     [CreateAssetMenu(fileName = "Bottle_", menuName = "Bottles/New Bottle Type", order = 0)]
     public class BottleType : ReceptacleType
     {
-        [Range(0,70)]
+        [Range(0,200)]
         public int fillCount = 50;
         [Space(20)]
         [SerializeField][ReadOnly]
