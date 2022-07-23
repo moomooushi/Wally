3c295be8e71fef275c6b1231ef7a3f65011e91ca
solidfry <ellioteweb@gmail.com>
2022-07-23 21:32:23 +1000

    dialogue animations are set up

--- Assets/Scripts/Utility/AnimateText.cs
+++ Assets/Scripts/Utility/AnimateText.cs
@@ -1,4 +1,4 @@
-using System;
+using DG.Tweening;
 using ScriptableObjects;
 using TMPro;
 using UnityEngine;
@@ -11,17 +11,30 @@ namespace Utility
         private Dialogues dialogues;
         [SerializeField]
         private TMP_Text textField;
-
+        DOTweenTMPAnimator _animator;
+        private Sequence _textSequence;
         private void Start()
         {
-            textField.text = "";
+            if(dialogues != null)
+                SetUpTextAnimations();
         }
 
-        private void Update()
+//        private void Update()
+//        {
+////            foreach (string s in dialogues.list)
+////            {
+////                textField.text = s;
+////            }
+//        }
+        void SetUpTextAnimations()
         {
+            _textSequence =  DOTween.Sequence();
+            _animator = new DOTweenTMPAnimator(textField);
+            textField.text = "";
+
             foreach (string s in dialogues.list)
             {
-                textField.text = s;
+                _textSequence.Append(textField.DOText(s,3)).AppendCallback(() => textField.text = "");
             }
         }
     }
